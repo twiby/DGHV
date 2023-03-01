5a95f6cb1bfc5ac941ec55b4cd8b5476a5a9cc90
twiby <t.balenbois@gmail.com>
2023-03-01 15:19:12 +0100

    Symetric bit encryption and decryption

--- src/dghv/mod.rs
+++ src/dghv/mod.rs
@@ -1,5 +1,5 @@
 use num_traits::Zero;
-use num_bigint::{BigInt, Sign};
+use num_bigint::{BigInt};
 use num_bigint::{ToBigInt, RandBigInt};
 
 #[cfg(test)]
@@ -16,17 +16,56 @@ fn key_gen() -> BigInt {
 	let mut min = 2.to_bigint().unwrap();
 	min = min.pow((ETA - 1).try_into().unwrap());
 
-	let b = rng.gen_bigint_range(&BigInt::zero(), &min);
+	let mut b = rng.gen_bigint_range(&BigInt::zero(), &min) + min;
 
-	return b + min;
+	if &b % 2 == BigInt::zero() { b += 1; }
+
+	return b;
 }
 
-struct EncryptedBit {
+struct SymetricallyEncryptedBit {
 	c: BigInt
 }
 
-impl EncryptedBit {
-	fn new(c: BigInt) -> EncryptedBit {
-		EncryptedBit{c:c}
+impl SymetricallyEncryptedBit {
+	fn new(c: BigInt) -> SymetricallyEncryptedBit {
+		SymetricallyEncryptedBit{c:c}
+	}
+
+	fn encrypt(message: bool, p: &BigInt) -> SymetricallyEncryptedBit {
+		let bit = match message {
+			true => 1.to_bigint().unwrap(),
+			false => BigInt::zero()
+		};
+
+		// r is between -p/4 and p/4
+		let min_r = -p/4;
+		let max_r = p/4;
+		let mut rng = rand::thread_rng();
+		let r = rng.gen_bigint_range(&min_r, &max_r);
+
+		// q is of the order of 2^(ETA^3)
+		let mut min_q = 2.to_bigint().unwrap();
+		min_q = min_q.pow((ETA - 1).try_into().unwrap());
+		let q = &min_q + rng.gen_bigint_range(&BigInt::zero(), &min_q);
+
+		dbg!((2*&r + &bit) + p);
+		return SymetricallyEncryptedBit::new(dbg!(dbg!(p*q) + dbg!(2*r + bit)));
+	}
+
+	fn decrypt(&self, p: &BigInt) -> bool {
+		let ret = (((&self.c+p/2) % p) - p/2) % 2;
+
+		if ret == BigInt::zero() {
+			return false;
+		} else if ret == 1.to_bigint().unwrap() {
+			return true;
+		}else if ret == -1.to_bigint().unwrap() {
+			return true;
+		} else {
+			panic!("Bit decrypt: mod 2 didn't work");
+		}
 	}
 }
+
+
--- src/dghv/test.rs
+++ src/dghv/test.rs
@@ -1,7 +1,7 @@
 use num_bigint::ToBigInt;
 
 
-use crate::dghv::{ETA, key_gen};
+use crate::dghv::{ETA, key_gen, SymetricallyEncryptedBit};
 
 #[test]
 fn key_gen_test() {
@@ -9,10 +9,33 @@ fn key_gen_test() {
 	let mut max  = 2.to_bigint().unwrap();
 
 	min = min.pow((ETA-1).try_into().unwrap());
-	max = max.pow(ETA.try_into().unwrap());
+	max = max.pow((ETA).try_into().unwrap());
 
 	let key = key_gen();
-	
+
+	assert!(&key % 2 == 1.to_bigint().unwrap());
 	assert!(key >= min);
 	assert!(key < max);
 }
+
+#[test]
+fn encrypt_test() {
+	let p = key_gen();
+
+	let _ = SymetricallyEncryptedBit::encrypt(true, &p);
+	let _ = SymetricallyEncryptedBit::encrypt(false, &p);
+}
+
+#[test]
+fn decrypt_test() {
+	let p = key_gen();
+
+	let t = SymetricallyEncryptedBit::encrypt(true, &p).decrypt(&p);
+	println!("{:?}", t);
+	// assert!(t == true);
+
+	let f = SymetricallyEncryptedBit::encrypt(false, &p).decrypt(&p);
+	println!("{:?}", f);
+	assert!(f == false);
+}
+
